09f0fdc44bb250b3a6fe0c176e2040fdd8cf1b18
nhm20 <nmo212182@gmail.com>
2024-09-04 11:45:52 +0530

    DependencyInjection  Spring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,11 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Laptop L1=new Laptop();
-        Student s1=new Student(102,"Ram",L1);
-        s1.doAssignment();
+        ApplicationContext ctx=new ClassPathXmlApplicationContext();
     }
 }
