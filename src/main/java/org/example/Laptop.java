60e56d0c07783a083b72bf5ef4b577531de62a2e
nhm20 <nmo212182@gmail.com>
2024-09-04 11:44:12 +0530

    DependencyInjection  Spring

--- src/main/java/org/example/Laptop.java
+++ src/main/java/org/example/Laptop.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class Laptop {
+    public void doCoding(){
+        System.out.println("Do the coding");
+    }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,4 +1,43 @@
 package org.example;
 
 public class Student {
+    private int Roll;
+    private String Name;
+    private Laptop  dell;
+    public void doAssignment(){
+        dell.doCoding();
+    }
+
+    public Student() {
+    }
+
+    public Student(int roll, String name, Laptop dell) {
+        Roll = roll;
+        Name = name;
+        this.dell = dell;
+    }
+
+    public int getRoll() {
+        return Roll;
+    }
+
+    public void setRoll(int roll) {
+        Roll = roll;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Laptop getDell() {
+        return dell;
+    }
+
+    public void setDell(Laptop dell) {
+        this.dell = dell;
+    }
 }
